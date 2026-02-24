533161a87ef9f5b341984efa72327e89804b4e08
Jair Lopez Perez <jair.lopez@ulv.edu.mx>
2026-02-24 10:47:54 -0600

    Jair Lopez Practica funciones

---
+++ funciones.py
@@ -0,0 +1,16 @@
+def hola(): print("Hola, mundo!")
+    
+def sumar_dos():
+    print(2 + 2)
+    
+def dame_un_cinco():
+    return 5
+
+def espacio():
+    print("\n")
+    
+def autor():
+    print("Creado por Jair")
+
+    
+    
